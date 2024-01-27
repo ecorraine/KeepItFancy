3373cd74d4d8e27cd9ed258822c0df6cf6f9496b
GO Grace <grace.sharon.go@gmail.com>
2024-01-28 01:56:55 +0900

    [MODIFIED]  - constant buffer data that determines tessellation status added in vertex shader to instruct if to just pass through or not

--- KeepItFancy/HLSL/VS_WorldPosition.hlsl
+++ KeepItFancy/HLSL/VS_WorldPosition.hlsl
@@ -17,18 +17,35 @@ struct VS_OUT
 	float3 worldPos : POSITION0;
 };
 
+cbuffer VSCommonData : register(b1)
+{
+	float	g_useTesselate;
+	float	g_TessFactor;
+	float2	padding;
+};
+
 VS_OUT main(VS_IN vin)
 {
 	VS_OUT vout = (VS_OUT)0;
 
-	// 座標変換
 	vout.pos		= float4(vin.pos, 1.0f);
-	vout.pos		= mul(vout.pos, world);
-	vout.worldPos	= vout.pos.xyz;
-	vout.pos		= mul(vout.pos, view);
-	vout.pos		= mul(vout.pos, proj);
 
-	vout.normal		= normalize(mul(vin.normal, (float3x3) world));
+	if (bool(!g_useTesselate))
+	{
+		// transform position || 座標変換
+		vout.pos = mul(vout.pos, world);
+		vout.worldPos = vout.pos.xyz;
+		vout.pos = mul(vout.pos, view);
+		vout.pos = mul(vout.pos, proj);
+
+		vout.normal = normalize(mul(vin.normal, (float3x3) world));
+	}
+	else
+	{
+		// pass through || 変換なし
+		vout.worldPos = vin.pos;
+		vout.normal = vin.normal;
+	}
 
 	vout.uv			= vin.uv;
 	vout.color		= vin.color;
